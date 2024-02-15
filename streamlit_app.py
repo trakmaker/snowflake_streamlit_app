1b61686a8d3dccbacb2c4b360e156bd12fb2f26e
KAKKIRENI RAVI TEJA <ravitejak966@gmail.com>
2024-02-15 09:39:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-
 import streamlit
-streamlit.title('My parents healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Hard boiled free rango egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
