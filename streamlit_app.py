2ecd5fc28d4dad6b63d087bbedddb1ab07cfd7c5
KAKKIRENI RAVI TEJA <ravitejak966@gmail.com>
2024-02-15 09:37:49 -0600

    fixed indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 
 import streamlit
 streamlit.title('My parents healthy diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Hard boiled free rango egg')
